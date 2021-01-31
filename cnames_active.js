81be8a085e1f4fd74cdda631dd3ccdf53d6bc721
Ömer Faruk Biçer <75714882+omerfarukbicer0446@users.noreply.github.com>
2021-02-01 02:20:43 +0300

    Create cnames_active.js

---
+++ cnames_active.js
@@ -0,0 +1,41 @@
+/*
+ ***** NEW SUBDOMAINS
+ * ***********************
+ *
+ * şu an için aşağıdaki alt alanlar etkin
+ * kendi GitHub Sayfanızı eklemek için bir çekme isteği gönderin
+ *
+ *
+ **** README
+ * ***********
+ *
+ * Anahtar (KEY): Talep ettiğiniz alt alan adı (örneğin: "foo", "foo.happies.fun" özel alan adını verir).
+ *
+ * Değer (Value): GitHub tarafından sağlanan normal alan (örneğin: Kullanıcı/Kuruluş Sayfası için "foo.github.io"
+ *                veya Proje Sayfası için "foo.github.io/bar").
+ *
+ * ÖNEMLİ:        GitHub Sayfasının sahibi olarak kendinizi yetkilendirmek için geçerli bir CNAME dosyası eklemiş olmanız gerekir
+ *                (istenen etki alanıyla, örneğin: "foo.js.org") deponuza.
+ *                İstenen alt alan adı, GitHub kullanıcı adınızla veya projenizin adıyla eşleşmelidir
+ *                depo. Netlik açısından istisnalar mümkündür (örneğin, projenizde
+ *                "foojs" adı "foo" isteyebilirsiniz).
+ *                Kişisel "tepki" veya "vue" çatalınız için bir istek başlatmadan önce okumalısınız
+ *                wiki'deki "adlandırma çatışmaları" bölümü!
+ *
+ * DİKKAT:        Lütfen bu dosyada herkes tarafından belirlenen biçimi takip edin. Girişlerde çift tırnak kullanılmalıdır,
+ *                iki nokta üst üste (anahtardan sonra) ve değer arasında boşluk olan ve her zaman alfabetik olmalıdır
+ *                sipariş. Bu dosyanın sağlanması için, son girdinin dışındaki her satırın arkasında bir virgül bulunmalıdır.
+ *                geçerli sözdizimidir.
+ *
+ *              Herhangi olası bir durumda biz sorumlu değiliz.
+ */
+
+var cnames_active = {
+  "": "smiley.github.io",
+  "docs": "smiley.github.io/docs",
+  /*
+   * Lütfen buraya kayıt eklemeyin.
+   * Çakışmaları öncelemek için alfabetik sıra ile ekleme yapın.
+   * <3
+   */
+}
